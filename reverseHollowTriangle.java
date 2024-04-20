4cac11e738e8cc3dcd7563c2309b0b6d8e7043ce
Shreyash Bhagat <102278480+Shreyash118@users.noreply.github.com>
2024-04-20 12:34:18 +0530

    Add files via upload

---
+++ reverseHollowTriangle.java
@@ -0,0 +1,30 @@
+package com.javaassignment1.in;
+
+import java.util.Scanner;
+
+public class reverseHollowTriangle {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        for(int i=0; i<3*n; i++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	        for(int i=0; i<n; i++){
+	            for(int j=0; j<i; j++){
+	                System.out.print(" ");
+	            }
+	            for(int j=0; j<=2*(n-i); j++){
+	                if(j == 0 || j == 2*(n-i)-2){
+	                    System.out.print("*");
+	                }
+	                else{
+	                    System.out.print(" ");
+	                }
+	            }
+	            System.out.println();
+	            }
+	            
+	 }
+
+}
